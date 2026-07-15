99d2a4ea3c8f9d73563e7027bded87e7c78c63c1
Hoài Nam <Namdo2003hp@gmail.com>
2026-07-16 02:01:52 +0700

    Sửa lỗi thông báo kiểm tra mật khẩu hoặc email trên shipper login

--- shipper/login/shipper_login.php
+++ shipper/login/shipper_login.php
@@ -1,26 +1,31 @@
 <?php
 require_once "../../db.php";
-require_once "login.php";
+
 $error = '';
 
 if (isset($_POST['login'])) {
+
     $email = trim($_POST['email']);
     $password = $_POST['password'];
 
-    $stmt = $conn->prepare("SELECT * FROM shipper WHERE email = ?");
+    $stmt = $conn->prepare("SELECT * FROM shipper WHERE email=?");
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($row = $result->fetch_assoc()) {
+
         if (password_verify($password, $row['password'])) {
+
             $_SESSION['shipper_id'] = $row['id'];
             $_SESSION['shipper_name'] = $row['name'];
 
             header("Location: ../shipper_dashboard.php");
-            exit;
+            exit();
         }
     }
 
     $error = "⚠️ Email hoặc mật khẩu không chính xác!";
 }
+
+require_once "login.php";
